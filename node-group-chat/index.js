390c1e525228d5c910380054270e0d07987b1e14
回头太难 <lan-cin@qq.com>
2019-06-10 21:46:17 +0800

    v1.0.0

--- node-group-chat/index.js
+++ node-group-chat/index.js
@@ -4,9 +4,27 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 app.get("/", function(req, res) {
-  res.send("hello, world");
+  res.render("index.ejs");
 });
 
 const server = http.listen(3000, function() {
   console.log("listen on *:3000");
+});
+
+io.sockets.on("connection", function(socket) {
+  socket.on("username", username => {
+    socket.username = username;
+    io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("is_online", "🔴 <i>" + socket.username + " left the chat..</i>");
+  });
+
+  socket.on("chat_message", function(message) {
+    io.emit(
+      "chat_message",
+      "<strong>" + socket.username + "</strong>: " + message
+    );
+  });
 });
